3b735cc05b5fc209e3749aee1d272b698918af98
Haner Camilo Perea Yara <hanercamilo@hotmail.com>
2019-02-02 23:10:26 -0500

    Queries

---
+++ fonts/montserrat/select.php
@@ -0,0 +1,9 @@
+<?php
+   
+   $db = new PDO('sqlite:https://gitlab.com/hanercamilo/teco/blob/master/WindowsStorage');
+   
+   $query = $_POST['query'];
+      
+   require_once('bodyselect.php');
+  
+?>
